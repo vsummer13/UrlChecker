0ef9623e6e25ce6046980df3eb43dbd23bd41b71
vsummer13 <vena_xyz@yahoo.com>
2018-12-12 17:16:08 -0800

    Add files via upload

---
+++ URLDemo.java
@@ -0,0 +1,9 @@
+import qual.*;
+
+public class URLDemo {
+    public static void main(String[] args) {
+        @U String myUrl = "www.google";
+
+	System.out.println("Your url is: " + myUrl);
+    }
+}
